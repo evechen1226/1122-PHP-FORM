097b5d0fccdf11cdcf59d007e52ee59c52850a88
evechen1226 <eveliving2021@gmail.com>
2024-03-04 11:37:57 +0800

    引入utils專案，解析字型檔，完成使用DOMPDF，將HTTP轉換為PDF

--- pdf-export.php
+++ pdf-export.php
@@ -1,35 +1,105 @@
-<?php include_once "db_export.php";
+<?php 
+require_once('./vendor/autoload.php');
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+include "db_export.php";
+
+$options = new Options();
+$options->set('defaultFont', 'NSTC');
+$dompdf = new Dompdf($options);
+//↑ DOMPDF 設定
+
 
 if (!empty($_POST)) {
-    echo "您希望匯出";
-    print_r($_POST['select']);
-    echo "以上資料";
+    // echo "您希望匯出";
+    // print_r($_POST['select']);
+    // echo "以上資料";
     //有問題
-    $dates = find('20200706', ['投票所編號' => $_POST['select']]);
-    echo ($dates);
+    // $dates = find('20200706', ['投票所編號' => $_POST['select']]);
+    // echo ($dates);
 
 
 
     $rows = all('20200706', " where `投票所編號` in ('" . join("','", $_POST['select']) . "') ");
-    print_r($rows);
-    $th =
-        $filename = date("Ymd") . rand(10000000, 99999999);
-    $file = fopen("./doc/{$filename}.csv", 'w');
+    // print_r($rows);
+
+    //$filename = date("Ymd") . rand(10000000, 99999999);
+    $filename = date("Ymd") . rand(10000000, 99999999) . ".pdf";
+
+    //$file = fopen("./doc/{$filename}.csv", 'w');
     //編碼 BIG5 轉 UTF8
-    fwrite($file, "\xEF\xBB\xBf");
+    //fwrite($file, "\xEF\xBB\xBf");
+
+    $html = "
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Document</title>
+        <style>
+        table{
+            border-collapse:collapse;
+            width:500px;
+        }
+        td{
+            border:1px solid #666;
+            padding:5px 12px;
+        }
+        tr:nth-child(odd){
+            background-color: #ccc;
+        }
+        </style>
+    </head>
+    <body> ";
+
+    $html .= "<table>";
 
     $chk = false;
 
     foreach ($rows as $row) {
         if (!$chk) {
             $cols = array_keys($row);
-            fwrite($file, join(",", $cols) . "`\r\n");
+            //fwrite($file, join(",", $cols) . "`\r\n");
+            $html .= "<tr>";
+            foreach ($cols as $col) {
+                $html .= "<td>";
+                $html .= $col;
+                $html .= "</td>";
+            }
+            $html .= "</tr>";
             $chk = true;
         }
-        fwrite($file, join(",", $row) . "`\r\n");
+        //fwrite($file, join(",", $row) . "`\r\n");
+        $html .= "<tr>";
+        foreach ($row as $r) {
+            $html .= "<td>";
+            $html .= $r;
+            $html .= "</td>";
+        }
+        $html .= "</tr>";
     }
-    fclose($file);
-    echo "<a href='./doc/{$filename}.csv' download>檔案已匯出，請點連結下載</a>";
+    $html .= "</table>
+    </body>
+</html>";
+
+    //fclose($file);
+
+    // 將 HTML 載入 Dompdf
+    $dompdf->loadHtml($html);
+
+    // 渲染 PDF（可選）
+    $dompdf->render();
+
+    // 將 PDF 輸出到文件或直接輸出到瀏覽器
+    $dompdf->stream("./doc/{$filename}", array('Attachment' => 0));
+    
+    //fclose($file);
+
+    //echo "<a href='./doc/{$filename}.csv' download>檔案已匯出，請點連結下載</a>";
+    echo "<a href='./doc/{$filename}'  download>檔案已匯出，請點此連結下載</a>";
 }
 
 ?>
@@ -48,10 +118,11 @@ if (!empty($_POST)) {
         background-color: black;
         color: white;
     }
-.form-check-input{
-    width: 100%;
-    height: 100%;
-}
+
+    .form-check-input {
+        width: 100%;
+        height: 100%;
+    }
 </style>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
